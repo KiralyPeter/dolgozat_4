3dbb03f2355dab4a306f7b4ce64d4cbfb37c45f7
Peter Kiraly <peter.kiral@gmail.com>
2023-03-05 15:34:01 +0100

    init

--- src/app.js
+++ src/app.js
@@ -1,1 +1,10 @@
-// init
+/*
+* File: app.js
+* Author: Király Péter
+* Copyright: 2023, Király Péter
+* Group: Szoft-1-2-E
+* Date: 2023-03-05
+* Github: https://github.com/KiralyPeter/javascript_dolgozat_4.git
+* Licenc: GNU GPL
+*/
+
